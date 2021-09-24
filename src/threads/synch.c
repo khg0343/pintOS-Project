85129480135c151196a301c43e1fd2a6129a3674
song <songsm921@postech.ac.kr>
2021-09-24 21:04:00 +0900

    sema_down priority check

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -68,7 +68,7 @@ sema_down (struct semaphore *sema)
   old_level = intr_disable ();
   while (sema->value == 0) 
     {
-      list_push_back (&sema->waiters, &thread_current ()->elem);
+      list_push_back (&sema->waiters, &thread_current ()->elem);//이때도 priority 고려해야함
       thread_block ();
     }
   sema->value--;
